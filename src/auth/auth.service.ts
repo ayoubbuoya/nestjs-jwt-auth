587d5ab261e2d5ae1c713a9ecd4a61538f1de326
Ayoub Amer <ayouub.ameur@gmail.com>
2024-07-13 10:22:47 +0100

    feat: compare passwords with bycrypt when login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,7 +14,9 @@ export class AuthService {
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(email);
 
-    if (user?.password !== pass) {
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (!isMatch) {
       throw new UnauthorizedException();
     }
 
